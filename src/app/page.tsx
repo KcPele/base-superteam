333e9fe1f61c4283e59a2429584606878a80def9
Kcpele <fidekg123@gmail.com>
2024-08-09 15:43:07 +0100

    feat:added wallet connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ConnectButton } from "@/components/connect button/ConnectButton";
 import { useAccount } from "wagmi";
 
 export default function Home() {
@@ -8,8 +9,8 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
-          {address}
         </p>
+        <ConnectButton />
       </div>
     </main>
   );
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,18 @@
 import { http, createConfig } from "wagmi";
 import { base, baseSepolia } from "wagmi/chains";
+import { coinbaseWallet } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [base, baseSepolia],
+  multiInjectedProviderDiscovery: false,
+  connectors: [
+    coinbaseWallet({
+      appName: "Super Team",
+      preference: "all", // set this to `all` to use EOAs as well
+      version: "4",
+    }),
+  ],
+  ssr: true,
   transports: {
     [base.id]: http(),
     [baseSepolia.id]: http(),
