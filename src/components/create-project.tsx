2209d132070b1b8af59fd7f038477d26c0cd633f
Kcpele <fidekg123@gmail.com>
2024-08-12 18:11:09 +0100

    fix error

--- src/components/create-project.tsx
+++ src/components/create-project.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 export const CreateProject = () => {
-
   return (
     <div>
       <div className="bg-basewhite px-36 pt-40 pb-20">
@@ -10,12 +9,12 @@ export const CreateProject = () => {
           <p className="text-lg text-baseblack font-semibold">
             Create Project/ Organization
           </p>
-          <p className="text-4xl font-bold text-baseblack">Let's Get Started</p>
+          <p className="text-4xl font-bold text-baseblack">
+            Let&apos;s Get Started
+          </p>
         </div>
       </div>
-      <div className="bg-basebgblack text-basewhite pb-20 py-16 px-36">
-        
-      </div>
+      <div className="bg-basebgblack text-basewhite pb-20 py-16 px-36"></div>
     </div>
   );
 };
